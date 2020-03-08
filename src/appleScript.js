d3cfe0bac9bdf298cf366c757627650d9ab53bf8
Magnus Poppe Wang <magnus.poppe.wang@gmail.com>
2020-03-08 10:20:15 +0100

    Electron tray app. Can launch apple script commands.

---
+++ src/appleScript.js
@@ -0,0 +1,28 @@
+const ps = require("child_process");
+
+const F2_1 = "113";
+const F2_2 = "120";
+const COMMAND = "command down";
+const OSASCRIPT = "osascript -e ";
+
+const performOSAScriptCommand = command => {
+  const result = ps.execSync(OSASCRIPT + command);
+  console.log(result.toString("utf-8"));
+};
+
+const targetDisplayModeActivate = () => {
+  performOSAScriptCommand(
+    `'tell application "System Events" to keystroke ${F2_2} using {${COMMAND}}'`
+  );
+  console.log(`Performed keystroke COMMAND+F2`);
+};
+
+const turnUpVolume = (volumePercent = 100) => {
+  performOSAScriptCommand(`'set volume output volume ${volumePercent}'`);
+  console.log(`VOLUME SET TO ${volumePercent}`);
+};
+
+module.exports = {
+  targetDisplayModeActivate,
+  turnUpVolume
+};
