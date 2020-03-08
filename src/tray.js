2f9aa6fc1c41abf5f41e51c454d4e143a2b303cd
Magnus Poppe Wang <magnus.poppe.wang@gmail.com>
2020-03-08 10:28:35 +0100

    Refactor for readability

--- src/tray.js
+++ src/tray.js
@@ -2,30 +2,38 @@ const { app, Menu, Tray, globalShortcut } = require("electron");
 const { targetDisplayModeActivate } = require("./appleScript");
 
 const SHORTCUT_FOR_ACTIVATING_DISPLAY_MODE = "CommandOrControl+F10";
-let tray = null;
 
-app.on("ready", () => {
-  tray = new Tray("./public/imac.png");
+const initiateTrayMenu = () => {
+  const tray = new Tray("./public/imac.png");
   const contextMenu = Menu.buildFromTemplate([
     {
-      click: () => targetDisplayModeActivate(),
+      click: targetDisplayModeActivate,
       label: "Use iMac as a display",
       type: "normal"
     }
   ]);
   tray.setToolTip("Simple tool for using iMac as a display");
   tray.setContextMenu(contextMenu);
+  return tray;
+};
 
+const registerKeyboardShortcut = () => {
   const ret = globalShortcut.register(
     SHORTCUT_FOR_ACTIVATING_DISPLAY_MODE,
-    () => {
-      targetDisplayModeActivate();
-    }
+    targetDisplayModeActivate
   );
-  console.log("Registerd shortcut: ", SHORTCUT_FOR_ACTIVATING_DISPLAY_MODE, ret);
+  console.log(
+    "Registerd shortcut: ",
+    SHORTCUT_FOR_ACTIVATING_DISPLAY_MODE,
+    ret
+  );
+};
+
+app.on("ready", () => {
+  initiateTrayMenu();
+  registerKeyboardShortcut();
 });
 
 app.on("will-quit", () => {
-  // Unregister a shortcut.
   globalShortcut.unregister(SHORTCUT_FOR_ACTIVATING_DISPLAY_MODE);
 });
